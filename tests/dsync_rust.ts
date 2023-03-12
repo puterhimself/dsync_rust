32fc7e2ef336b15477da85cfc28433d2fcc081f6
puterhimself <22245429+puterhimself@users.noreply.github.com>
2023-03-13 04:20:51 +0530

    yarn add spl token

--- tests/dsync_rust.ts
+++ tests/dsync_rust.ts
@@ -1,6 +1,14 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { IDL } from "../target/types/dsync_rust";
 import { DsyncRust } from "../target/types/dsync_rust";
+// import { IDL } from "../target/types/anchor_escrow";
+import { PublicKey, SystemProgram, Transaction, Connection, Commitment } from "@solana/web3.js";
+import { TOKEN_PROGRAM_ID, createMint, createAccount, mintTo, getAccount } from "@solana/spl-token";
+import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
+import { assert, expect, should } from "chai";
+
+
 
 describe("dsync_rust", () => {
   // Configure the client to use the local cluster.
