08efe86c11e86d82af7874ae882eb00e1a516d4e
puterhimself <22245429+puterhimself@users.noreply.github.com>
2023-03-12 22:18:52 +0530

    intialize job

--- programs/dsync_rust/src/lib.rs
+++ programs/dsync_rust/src/lib.rs
@@ -30,7 +30,7 @@ pub mod dsync_rust {
     // const (TOKEN_AUTHORITY, _bump) = Pubkey::find_program_address(&[AUTHORITY_SEED.as_bytes()], ctx.program_id);
 
     pub fn initialize_client(ctx: Context<InitializeClient>, _client: Pubkey) -> Result<()> {
-        let client = &mut ctx.accounts.client;  
+        let client = &mut ctx.accounts.client;
         client.owner = _client; // added for convinience otherwise just bump with seed is enough.
         client.job_count = 0;
         client.bump = *ctx.bumps.get(CLIENT_SEED).unwrap();
@@ -39,31 +39,39 @@ pub mod dsync_rust {
 
     pub fn initialize_job(
         ctx: Context<InitializeJob>,
-        description: String,
-        price: u64,
-        deadline: i64,
+        _description: String,
+        _price: u64,
+        _is_native: bool,
+        _deadline: i64,
     ) -> Result<()> {
-        let _task = &mut ctx.accounts.task;
         let _client = &mut ctx.accounts.client;
+        let _task = &mut ctx.accounts.job;
+        let _valut = &mut ctx.accounts.vault;
+
         _client.job_count += 1;
 
         _task.bump = *ctx.bumps.get(JOB_SEED).unwrap();
         _task.index = _client.job_count;
         _task.client = _client.owner;
+
         _task.state = JobState::PENDING;
-        _task.job_description = description;
-        _task.publish_date = Clock::get().unwrap().unix_timestamp;
-        _task.deadline = deadline;
-        _task.price = price;
-        _task.is_native = false;
+        _task.job_description = _description;
+
+        _task.validator = _client.owner;
+        _task.submission_count = 0;
+        _task.price = _price;
         _task.currency = ctx.accounts.currency.to_account_info().key.clone();
-        _task.client_token_account = ctx
-            .accounts
-            .client_token_account
-            .to_account_info()
-            .key
-            .clone();
+        _task.is_native = _is_native;
+
+        _task.publish_date = Clock::get().unwrap().unix_timestamp;
+        _task.deadline = _deadline;
         _task.vault_token_account = ctx.accounts.vault.to_account_info().key.clone();
+        // _task.client_token_account = ctx
+        //     .accounts
+        //     .client_token_account
+        //     .to_account_info()
+        //     .key
+        //     .clone();
 
         Ok(())
     }
@@ -145,7 +153,6 @@ pub mod dsync_rust {
         )?;
         Ok(())
     }
-
 }
 
 #[derive(Accounts)]
@@ -166,32 +173,33 @@ pub struct InitializeClient<'info> {
 }
 
 #[derive(Accounts)]
+// #[instruction(currency: Pubkey)]
 pub struct InitializeJob<'info> {
     #[account(mut)]
-    pub owner: Signer<'info>,
-    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(mut, seeds = [CLIENT_SEED.as_bytes(), client.owner.as_ref()], bump=client.bump)]
     pub client: Box<Account<'info, Client>>,
-    pub currency: Account<'info, Mint>,
     #[account(
         init,
-        seeds = [JOB_SEED.as_bytes(), owner.key.as_ref(), &client.job_count.to_le_bytes()],
+        seeds = [JOB_SEED.as_bytes(), client.owner.as_ref(), &client.job_count.to_le_bytes()],
         bump,
-        payer = owner,
+        payer = signer,
         space = Job::SPACE,
-        // authority = task.key
     )]
-    pub task: Box<Account<'info, Job>>,
+    pub job: Box<Account<'info, Job>>,
     #[account(
         init,
-        payer = owner,
-        seeds = [VAULT_SEED.as_bytes(), &task.job_id.as_ref()],
+        seeds = [VAULT_SEED.as_bytes(), &job.to_account_info().key.clone().as_ref()],
         bump,
+        payer = signer,
         token::mint = currency,
         token::authority = vault
     )]
     pub vault: Box<Account<'info, TokenAccount>>,
-    pub client_token_account: Box<Account<'info, TokenAccount>>,
 
+    #[account(constraint = vault.mint == currency.to_account_info().key.clone())]
+    pub currency: Account<'info, Mint>,
+    // pub client_token_account: Box<Account<'info, TokenAccount>>,
     /// CHECK: This is not dangerous because we don't read or write from this account
     pub system_program: Program<'info, System>,
     /// CHECK: This is not dangerous because we don't read or write from this account
@@ -253,7 +261,6 @@ pub struct PostSubmission<'info> {
     )]
     pub submission: Box<Account<'info, Submission>>,
     pub system_program: Program<'info, System>,
-
     // #[account(mut)]
     // pub vault: Box<Account<'info, TokenAccount>>,
     // pub client_token_account: Box<Account<'info, TokenAccount>>,
@@ -287,46 +294,44 @@ pub struct ClaimReward<'info> {
     pub token_program: Program<'info, Token>,
 }
 
-
 #[account]
 pub struct Client {
     pub bump: u8,
     pub owner: Pubkey,
     pub job_count: u64,
-
     /*
      *EXTRA VALUES that can be added
-        * pub client_name: String,
-        * pub total_jobs: u64,
-        * pub total_staked: u128,
+     * pub client_name: String,
+     * pub total_jobs: u64,
+     * pub total_staked: u128,
      */
 }
 
 #[account]
 pub struct Job {
-    pub bump: u8,
-    pub index: u64,
-    pub client: Pubkey,
+    pub bump: u8,       // 1 byte
+    pub index: u64,     // 8 bytes
+    pub client: Pubkey, // 32 bytes
     // pub client_account: Pubkey,
-    pub job_id: String,
-    pub state: JobState,
-    pub job_description: String,
+    // pub job_id: String, // 4 + 1 byte = 1 char | 4+(length of string) = 14 bytes
+    pub state: JobState,         // 1 + 4 + 9 = 14 bytes
+    pub job_description: String, // 4 + 1 byte = 1 char | 4+(length of string) = 54 bytes
 
-    pub winner: Pubkey,
-    pub validator: Pubkey,
-    pub submission_count: u64,
+    pub winner: Pubkey,        // 32 bytes
+    pub validator: Pubkey,     // 32 bytes
+    pub submission_count: u64, // 8 bytes
 
-    pub price: u64,
-    pub currency: Pubkey,
-    pub is_native: bool,
+    pub price: u64,       // 8 bytes
+    pub currency: Pubkey, // 32 bytes
+    pub is_native: bool,  // 1 byte
 
-    pub publish_date: i64,
-    pub deadline: i64,
+    pub publish_date: i64, // 8 bytes
+    pub deadline: i64,     // 8 bytes
 
-    pub client_token_account: Pubkey,
-    pub validator_token_account: Pubkey,
-    pub winner_token_account: Pubkey,
-    pub vault_token_account: Pubkey,
+    pub vault_token_account: Pubkey, // 32 bytes
+                                     // pub client_token_account: Pubkey,   // 32 bytes
+                                     // pub validator_token_account: Pubkey,    // 32 bytes
+                                     // pub winner_token_account: Pubkey,  // 32 bytes
 }
 
 #[account]
@@ -341,8 +346,6 @@ pub struct Submission {
     pub is_validated: bool,
     pub is_winner: bool,
 }
-
-
 
 impl Default for JobState {
     fn default() -> Self {
@@ -382,9 +385,9 @@ impl<'info> CancelJob<'info> {
             &self.task.bump.to_le_bytes(),
         ];
         CpiContext::new_with_signer(
-            self.token_program.to_account_info(), 
+            self.token_program.to_account_info(),
             cpi_accounts,
-            &[seeds.as_slice()]
+            &[seeds.as_slice()],
         )
     }
 }
@@ -402,9 +405,9 @@ impl<'info> ClaimReward<'info> {
             &self.task.bump.to_le_bytes(),
         ];
         CpiContext::new_with_signer(
-            self.token_program.to_account_info(), 
+            self.token_program.to_account_info(),
             cpi_accounts,
-            &[seeds.as_slice()]
+            &[seeds.as_slice()],
         )
     }
 }
@@ -414,8 +417,9 @@ impl Client {
 }
 
 impl Job {
+    const SPACE: usize = 8 + (5 * 32) + (8 * 5) + 14 + 14 + 54 + 1 + 1 + 10;
     // const SPACE: usize = 1 + 8 + 32 + 32 + 32 + 32 + (4 + 10) + 8 + (1 + 32) + (4 + 20) + 8 + 8 + 8 + 1 + (32 * 5) + 8;
-    const SPACE: usize = 1 + (4 + 10) + (1 + 32) + (4 + 20) + 1 + (32 * 5 + 4) + (8 * 6);
+    // const SPACE: usize = 1 + (4 + 10) + (1 + 32) + (4 + 20) + 1 + (32 * 5 + 4) + (8 * 6);
     // const SEEDS = [owner.key.as_ref(), CLIENT_SEED.as_bytes(), &client.job_count.to_le_bytes()]
 }
 
