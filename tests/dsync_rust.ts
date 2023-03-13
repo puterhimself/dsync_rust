b12629c1bd989b482a80e3a8657d16fb266107b3
puterhimself <22245429+puterhimself@users.noreply.github.com>
2023-03-13 14:03:35 +0530

    testing

--- tests/dsync_rust.ts
+++ tests/dsync_rust.ts
@@ -12,13 +12,47 @@ import { assert, expect, should } from "chai";
 
 describe("dsync_rust", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  // Use Mainnet-fork for testing
+  const commitment: Commitment = "confirmed";
+  const connection = new Connection("https://rpc-mainnet-fork.epochs.studio", {
+    commitment,
+    wsEndpoint: "wss://rpc-mainnet-fork.epochs.studio/ws",
+  });
+  const options = anchor.AnchorProvider.defaultOptions();
+  const wallet = NodeWallet.local();
+  const provider = new anchor.AnchorProvider(connection, wallet, options);
+
+  const CLIENT_SEED = "DSYNC_CLIENT";
+  const VAULT_SEED = "DSYNC_VAULT";
+  const AUTHORITY_SEED = "DSYNC_AUTHORITY";
+  const JOB_SEED = "DSYNC_JOB";
+  const SUBMISSION_SEED = "DSYNC_SUBMISSION";
+
+  enum JobState {
+    PENDING,
+    PUBLISHED,
+    ACTIVE,
+    VALIDATED,
+    COMPLETED,
+    CANCELED,
+  }
+  anchor.setProvider(provider);
+  // anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.DsyncRust as Program<DsyncRust>;
 
+  let mintA = null as PublicKey;
+  let mintB = null as PublicKey;
+  
+  let initializerTokenAccountA = null as PublicKey;
+  let initializerTokenAccountB = null as PublicKey;
+  
+  let workerTokenAccountA = null as PublicKey;
+  let workerTokenAccountB = null as PublicKey;
+
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.initializeClient().rpc();
     console.log("Your transaction signature", tx);
   });
 });
